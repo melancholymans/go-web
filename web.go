3b6d8afcdc8fa23cbc8bb39b3dba4ce1ea789d40
joshua <kjkq26993@gaia.eonet.ne.jp>
2022-09-03 09:46:10 +0900

    テンプレートを使う

--- web.go
+++ web.go
@@ -1,9 +1,29 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"text/template"
 )
 
 func main() {
-	http.ListenAndServe("", http.FileServer(http.Dir(".")))
+	html := `<html>
+	<body>
+	<h1>HELLO</h1>
+	<p>This is sample message</p>
+	</body>
+	</html>`
+	tf, err := template.New("index").Parse(html)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	hh := func(w http.ResponseWriter, rq *http.Request) {
+		err = tf.Execute(w, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	http.HandleFunc("/hello", hh)
+	http.ListenAndServe("", nil)
 }
